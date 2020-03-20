e089c6f262f4055d3c8a9e194d97123ce45f296e
xyzdxdydz <mastermind2012@hotmail.co.th>
2020-03-20 20:58:22 +0700

    first

---
+++ templates/push.js
@@ -0,0 +1,20 @@
+var push = require('web-push')
+
+let vapidKeys = {
+    publicKey: 'BPsIqVfCuke-WXVfHgaKQupC2e0_m2GevdDDhdRp_cABo1N5bRJN4E1JGL3k1loI8AVE9vA9jeOsarJTZ8MF8gw',
+    privateKey: 'T8rhqYL_FSgyi9iePkwwwZlNeJ1joCb2OV9kkxol-0I'    
+  }
+
+push.setVapidDetails('mailto:test@code.co.uk', vapidKeys.publicKey, vapidKeys.privateKey)
+
+let sub = {
+    endpoint:
+    "https://fcm.googleapis.com/fcm/send/cb3R1rE_N6Q:APA91bFspfxsrFbk_vGvuaOZrsEwM378Jus8B7VOCGxPLaJIa78BYlBJ4NOekRJwgrzDUuA-0daWV-zpqwOhXmw7AAsbQhiVSt5rHE2816n4tDkPEhLWdLY-Z7he8SpY-xKwENyd3gbV",
+    expirationTime: null,
+    keys: {
+        p256dh:"BCFzmKMhkbXPtm9fr2Hl_fTpTyYZSNUWN3cc9gLXVd4WfWviHv64oNzeb8NQIEmqpZuAa0dJQQG9ADhIgn00BsA",
+        auth:"avuX5OtvLlHZZWmKhbIFTg"
+    }
+};
+
+push.sendNotification(sub, 'test message')
---
+++ templates/sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('push', function(e) {
+  var options = {
+    body: 'What is this',
+    icon: 'images/example.png',
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: '2'
+    },
+    actions: [
+      {
+          action: 'explore', 
+          title: 'Explore this new world',
+          icon: 'images/checkmark.png'
+      },
+      { action: 'close', title: 'Close',
+        icon: 'images/xmark.png'},
+    ]
+  };
+  e.waitUntil(
+    self.registration.showNotification('Hello world!', options));
+});
